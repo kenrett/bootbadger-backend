e3cc5fbccbcb3b988ca1bf231f10be8234f7166a
BootCoder <bootcoder@gmail.com>
2017-12-07 16:38:41 -0800

    add check for lookup pre vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,11 +1,13 @@
 class VotesController < ApplicationController
 
   def create
-    @boot = Boot.find(params[:boot_id])
     @submitted_by = Boot.find_by_token(params[:token])
+    @boot = Boot.find(params[:boot_id])
     @slogan = Slogan.find(params[:slogan_id])
-    @vote = @slogan.votes.create(boot_id: @submitted_by.id)
-    render json: @slogan
+    if @submitted_by && @boot && @slogan
+      @vote = @slogan.votes.create(boot_id: @submitted_by.id)
+      render json: @slogan
+    end
   end
 
   private
