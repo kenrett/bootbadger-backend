500298d4a61f89318f9f36aee89294251b263e0e
BootCoder <bootcoder@gmail.com>
2017-12-07 16:46:41 -0800

    error handling on vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -5,8 +5,12 @@ class VotesController < ApplicationController
     @boot = Boot.find(params[:boot_id])
     @slogan = Slogan.find(params[:slogan_id])
     if @submitted_by && @boot && @slogan
-      @vote = @slogan.votes.create(boot_id: @submitted_by.id)
-      render json: @slogan
+      @vote = @slogan.votes.new(boot_id: @submitted_by.id)
+      if @vote.save
+        render json: @slogan
+      else
+        render json: @vote.errors
+      end
     end
   end
 
