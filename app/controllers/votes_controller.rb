6802b1cd25dd7a2f1bf8a906d27558b4f4aa290e
BootCoder <bootcoder@gmail.com>
2017-12-07 18:03:39 -0800

    bug on vote update

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -23,7 +23,7 @@ class VotesController < ApplicationController
 
   def update
     @submitted_by = Boot.find_by_token(vote_params['token'])
-    @vote.find(boot_id: @submitted_by.id, slogan_id: params[:slogan_id])
+    @vote = Vote.find(boot_id: @submitted_by.id, slogan_id: params[:slogan_id])
     @vote.destory
     status 200
   end
